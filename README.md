9e55d792635aaa304f4d20e562e68018c109e74f
xBlueCode <xbluecode@localhost.localdomain>
2018-05-22 05:43:24 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Java Tutorial: Spring 5 & Hibernate 5 (Configuration & Transaction Management)
---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    java
+}
+
+group = "xbc.tut"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+
+    compile("org.springframework", "spring-orm", "5.0.6.RELEASE");
+    compile("org.springframework", "spring-context", "5.0.6.RELEASE");
+
+    compile("org.hibernate:hibernate-agroal:5.3.0.Final")
+    compile("com.h2database:h2:1.4.197")
+
+    //testCompile("junit", "junit", "4.12")
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
